1a1b3c2746233edd644077253d0243b531bde09f
Jinzyy <672021177@student.uksw.edu>
2024-10-07 16:56:08 +0700

    feat: Web-Vitals

--- src/index.js
+++ src/index.js
@@ -2,6 +2,11 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App";
+import { getCLS, getFID, getLCP } from "web-vitals";
 
 const root = createRoot(document.getElementById("root"));
 root.render(<App />);
+
+getCLS(console.log);
+getFID(console.log);
+getLCP(console.log);
